485302dfa402a2ceeb6e2e1815c6632f8029b5d3
Marcus Gaspar <marcusgaspar@hotmail.com>
2020-07-02 14:36:02 -0300

    Remove temporary changes from master

--- WorkplaceHealthAndSafetyDemo/azuremaps/src/AzureMapsDemo.Web/Services/MapsStorageService.cs
+++ WorkplaceHealthAndSafetyDemo/azuremaps/src/AzureMapsDemo.Web/Services/MapsStorageService.cs
@@ -130,10 +130,10 @@ namespace AzureMapsDemo.Web.Services
     {
       return new List<UserLocationModel>
       {
-        //GetFakeUserByName("John", latitude, longitude),
-        //GetFakeUserByName("Jake", latitude, longitude),
-        //GetFakeUserByName("Sue", latitude, longitude),
-        //GetFakeUserByName("Pete", latitude, longitude)
+        GetFakeUserByName("John", latitude, longitude),
+        GetFakeUserByName("Jake", latitude, longitude),
+        GetFakeUserByName("Sue", latitude, longitude),
+        GetFakeUserByName("Pete", latitude, longitude)
       };
     }
 
