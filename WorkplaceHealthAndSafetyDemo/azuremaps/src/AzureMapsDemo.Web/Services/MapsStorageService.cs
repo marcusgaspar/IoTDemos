a063554c2b8bf3495882ce7fd24d9f092374bcd9
Marcus Gaspar <marcusgaspar@hotmail.com>
2020-07-06 13:18:41 -0300

    fix CRLF

--- WorkplaceHealthAndSafetyDemo/azuremaps/src/AzureMapsDemo.Web/Services/MapsStorageService.cs
+++ WorkplaceHealthAndSafetyDemo/azuremaps/src/AzureMapsDemo.Web/Services/MapsStorageService.cs
@@ -130,7 +130,7 @@ namespace AzureMapsDemo.Web.Services
     {
       return new List<UserLocationModel>
       { 
-        //commented for the POC
+        //commented for the POC 2
         //GetFakeUserByName("John", latitude, longitude),
         //GetFakeUserByName("Jake", latitude, longitude),
         //GetFakeUserByName("Sue", latitude, longitude),
